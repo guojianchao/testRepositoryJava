64701ff97ca75ee752c41fc3afca0d571dd138a9
guojianchao <guojinchao@837776282@qq.com>
2013-06-28 09:52:44 +0800

    Signed-off-by: guojianchao<guojinchao@837776282@qq.com>

---
+++ test/src/demo/MyProperties.java
@@ -0,0 +1,197 @@
+package demo;
+
+import java.io.*;
+import java.util.*;
+
+public class MyProperties extends ArrayList {
+	private static final long UID = 1L;
+
+	private String code = "GBK"; // 设置编码方式
+
+	private String fileName; // 文件名包括路径和后缀
+
+	public static void main(String[] args) throws Exception {// java程序主入口处
+		String path = "D:/proper.properties";
+		MyProperties proper = new MyProperties(path, "GBK");
+		proper.setTitle("测试用Properties设置键-值对的值");
+		proper.setProperties("studentName", "珍妮");
+		proper.setNotes("studentName", "学生姓名"); // 设置用户名备注
+		proper.setProperties("room", "三年一班");
+		proper.setNotes("room", "所在年级"); // 设置密码备注
+		proper.setProperties("score", "98.5");
+		proper.setNotes("score", "数学分数"); // 设置用户地址备注
+		proper.saveFile();// 将内容写入属性文件
+		System.out.println(readFile(path, "GBK")); // 读取属性文件内容
+		proper.getValue();// 读取项目中的属性文件的键值
+	}
+
+	public String getFileName() { // 获得文件名字
+		return fileName;
+	}
+
+	private void setFileName(String fileName) { // 设置文件名字
+		this.fileName = fileName;
+	}
+
+	// 带参数的构造方法
+	public MyProperties(String fileName, String code) {
+		try {
+			this.setFileName(fileName); // 设置文件
+			// 调用方法设置编码方式
+			this.setCharacterEncoding(code);
+			if (!isExist(fileName)) // 判断文件是否存在
+				this.writeFile("");
+			// 调用方法将元素放入集合中
+			this.addAll(Arrays.asList(readFile(fileName, code).split("\n")));
+		} catch (Exception ex) { // 捕获异常
+			ex.printStackTrace();
+		}
+	}
+
+	private void setCharacterEncoding(String code)
+			throws UnsupportedEncodingException { // 设置编码方式
+		new String("".getBytes("iso8859_1"), code);// 编码转换
+		this.code = code;
+	}
+
+	public static boolean isExist(String fileName) { // 判断文件是否存在
+		return new File(fileName).isFile(); // 是否是一个文件
+	}
+
+	public static String readFile(String fileName, String code)
+			throws IOException { // 读取信息
+		StringBuffer sb = new StringBuffer(); // 创建字符缓冲流
+		BufferedReader in = new BufferedReader( // 创建缓冲读对象
+				new FileReader(fileName));
+		String s;
+		while ((s = in.readLine()) != null) { // 循环读取文件中的信息
+			sb.append(s); // 字符串拼接
+			sb.append("\n"); // 换行
+		}
+		in.close(); // 释放资源
+		return sb.toString(); // 返回读取的字符串
+	}
+
+	public void writeFile(String proper) throws IOException { // 字符串写入文件
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+				fileName))); // 创建文本输出流打印对象
+		out.print(proper); // 将字符串写入指定文件
+		out.close(); // 释放资源
+	}
+
+	public void saveFile() throws IOException { // 数据保存到文件中
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+				fileName))); // 创建文本输出流打印对象
+		String tmp;
+		for (int i = 0; i < size(); i++) { // 循环显示集合信息输出到控制台
+			tmp = get(i) + "";
+			out.println(tmp);
+		}
+		out.close();
+	}
+
+	public void setProperties(String key, String val) { // 设置Properties键值
+		int pro = lookForKey(key);
+		if (pro >= 0)
+			this.set(pro, key + "=" + val);
+		else
+			this.add(key + "=" + val);
+	}
+
+	public int lookForKey(String key) { // 查找键序号
+		try {
+			String temp;
+			for (int i = 0; i < size(); i++) { // 循环显示集合信息
+				temp = get(i) + "";
+				temp = new String(temp.getBytes("iso8859_1"), code);// 编码转换
+				if (temp.indexOf(key) == 0) { // 没有找到键值
+					return i;
+				}
+			}
+		} catch (Exception e) { // 捕获异常
+		}
+		return -1;
+	}
+
+	public void setNotes(String key, String memo) { // 增加备注
+		if ("".equals(key)) {
+			this.add("#" + memo);
+			return;
+		}
+		String temp;
+		int result = lookForKey(key);
+		if (result == -1) { // 如果没有找到
+			this.add("#" + memo);
+			this.add(key + "=");
+		} else {
+			int position = result - 1;
+			if (position < 0) {
+				this.add(position, "#" + memo);
+			} else {
+				temp = this.get(position) + " ";
+				if ("#".equals(temp.substring(0, 1))) // 判断截取值是否与#相同
+					this.set(position, "#" + memo);
+				else {
+					this.add(position + 1, "#" + memo);
+				}
+			}
+		}
+	}
+
+	public void setTitle(String title) { // 设置注释内容
+		String tmp = this.get(0) + "";
+		if (tmp == null || tmp.length() == 0)
+			tmp = "";
+		else
+			tmp = tmp.substring(0, 1); // 截取第一个元素
+		if ("#".equals(tmp)) // 判断第一个元素是否是#
+			this.set(0, "#" + title); // 增加注释内容
+		else {
+			this.add(0, "");
+			this.add(0, "#" + title);
+		}
+	}
+
+	public String getProperties(String key) { // 获取键对应的值
+		return getProperties(key, "");
+	}
+
+	public String getProperties(String key, String defaultStr) {
+		String temp, result;
+		try {
+			for (int i = 0; i < size(); i++) { // 循环显示集合信息
+				temp = get(i) + ""; // 获得元素
+				temp = new String(temp.getBytes("iso8859_1"), code);// 编码转换
+				if (temp.indexOf(key) == 0) { // 找到指定的键
+					result = temp.substring(key.length() + 1);// 截取获得键对应的值
+					return result;
+				}
+			}
+		} catch (Exception e) { // 捕获异常
+		}
+		return defaultStr;
+	}
+
+	public void getValue() {// 获得项目中的properties文根据键取得相应的值
+		try {
+			InputStream in = new BufferedInputStream(new FileInputStream(
+					"proper.properties")); // 创建输入流对象
+			Properties p = new Properties(); // 创建属性对象
+			p.load(in); // 加载输入流对象
+			String s = (String) p.get("studentName"); // 获得键值
+			if(s!=null){
+				// 编码转换
+				String name = new String(s.getBytes("iso8859_1"), "GBK");
+				System.out.println("输出studentName的值：" + name);
+
+				String a = (String) p.get("room");
+				String room = new String(a.getBytes("iso8859_1"), "GBK");
+				System.out.println("输出room的值：" + room);
+			}
+			
+		} catch (IOException e) { // 捕获异常
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ test/src/test/DoXMLString.java
@@ -0,0 +1,109 @@
+package test;  
+import java.io.IOException;  
+import java.io.InputStream;  
+import java.net.MalformedURLException;  
+import java.net.URL;  
+import java.net.URLConnection;  
+import java.io.FileNotFoundException;  
+import java.io.FileOutputStream;  
+import java.io.PrintWriter;  
+import org.w3c.dom.Document;  
+import org.w3c.dom.DOMException;  
+import org.xml.sax.SAXException;  
+import javax.xml.parsers.DocumentBuilder;  
+import javax.xml.parsers.DocumentBuilderFactory;  
+import javax.xml.parsers.ParserConfigurationException;  
+import javax.xml.transform.OutputKeys;  
+import javax.xml.transform.Transformer;  
+import javax.xml.transform.TransformerConfigurationException;  
+import javax.xml.transform.TransformerException;  
+import javax.xml.transform.TransformerFactory;  
+import javax.xml.transform.dom.DOMSource;  
+import javax.xml.transform.stream.StreamResult;  
+/*** 
+ *  
+ * @author qiailin 漆艾林 
+ *  
+ *         2011年1月19日 13:30:47 
+ *  
+ *         DomXMLString.java 
+ *  
+ *         概述：纯java方式访问远程WebService接口返回的xml格式的数据保存在本地 
+ */  
+public class DoXMLString {  
+    private static String SERVICES_HOST = "www.webxml.com.cn";  
+    // 远程WebService接口url  
+    private static String NETDATA_URL = "http://webservice.webxml.com.cn/WebServices/WeatherWS.asmx/getRegionProvince";  
+    // 访问远程WebService接口返回的xml格式的数据保存在本地的绝对路径  
+    private static String LOCAL_PC_SAVEFILE_URL = "netDataToLocalFile.xml";  
+    private DoXMLString() {  
+    }  
+    public static void main(String[] args) throws Exception {  
+        Document document = getProvinceCode(NETDATA_URL);  
+        helloOK(document, LOCAL_PC_SAVEFILE_URL);  
+    }  
+    /* 返回一个Document对象 */  
+    public static Document getProvinceCode(String netXMLDataURL) {  
+        Document document = null;  
+        DocumentBuilderFactory documentBF = DocumentBuilderFactory.newInstance();  
+        documentBF.setNamespaceAware(true);  
+        try {  
+            DocumentBuilder documentB = documentBF.newDocumentBuilder();  
+            InputStream inputStream = getSoapInputStream(netXMLDataURL); // 具体webService相关  
+            document = documentB.parse(inputStream);  
+            inputStream.close();  
+        } catch (DOMException e) {  
+            e.printStackTrace();  
+            return null;  
+        } catch (ParserConfigurationException e) {  
+            e.printStackTrace();  
+            return null;  
+        } catch (SAXException e) {  
+            e.printStackTrace();  
+            return null;  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+            return null;  
+        }  
+        return document;  
+    }  
+    /* 返回InputStream对象 */  
+    public static InputStream getSoapInputStream(String url) {  
+        InputStream inputStream = null;  
+        try {  
+            URL urlObj = new URL(url);  
+            URLConnection urlConn = urlObj.openConnection();  
+            urlConn.setRequestProperty("Host", SERVICES_HOST); // 具体webService相关  
+            urlConn.connect();  
+            inputStream = urlConn.getInputStream();  
+        } catch (MalformedURLException e) {  
+            e.printStackTrace();  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        }  
+        return inputStream;  
+    }  
+    /* 访问远程(WebService)xml数据后返回的xml格式字符串并生成为本地文件 */  
+    public static void helloOK(Document document, String savaFileURL) {  
+        TransformerFactory transF = TransformerFactory.newInstance();  
+        try {  
+            Transformer transformer = transF.newTransformer();  
+            DOMSource source = new DOMSource(document);  
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");  
+            transformer.setOutputProperty(OutputKeys.INDENT, "YES");  
+              
+            PrintWriter pw = new PrintWriter(new FileOutputStream(savaFileURL));  
+            StreamResult result = new StreamResult(pw);  
+            transformer.transform(source, result);  
+            System.out.println("生成xml文件成功!");  
+        } catch (TransformerConfigurationException e) {  
+            System.out.println(e.getMessage());  
+        } catch (IllegalArgumentException e) {  
+            System.out.println(e.getMessage());  
+        } catch (FileNotFoundException e) {  
+            System.out.println(e.getMessage());  
+        } catch (TransformerException e) {  
+            System.out.println(e.getMessage());  
+        }  
+    }  
+} 
